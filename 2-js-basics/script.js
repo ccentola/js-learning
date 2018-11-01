f81be9b14d5749f68c2a738f07a61d5ff8f9ab3b
Carl Centola <carl.centola@gmail.com>
2018-11-01 16:22:51 -0400

    basic operators

--- 2-js-basics/script.js
+++ 2-js-basics/script.js
@@ -1,40 +1,44 @@
-/* Variable Mutation and Type Coercion */
+/* Basic Operators */
 
-var firstName = 'Carl';
-var age = 29;
+// calculate birth year
+var yearCarl = 2018 - 29;
+console.log(yearCarl);
 
-// js automatically coerces types
-console.log(firstName+ ' '+age);
+var yearCarl = 2018 - 29;
+var yearJohn = 2018 - 32;
+console.log(yearCarl);
+console.log(yearJohn);
 
-// declare many variables on a single line
-var job, isMarried;
-job = 'Teacher';
-isMarried = false;
+// should reuse vars to avoid repetition
+var year, yearCarl, yearJohn
+year = 2018;
+yearCarl = year - 29;
+yearJohn = year - 32;
 
-console.log(
-    firstName +
-    ' is a ' + 
-    age + 
-    ' year old ' + 
-    job + 
-    '. Is he married? ' + 
-    isMarried
-);
+console.log(yearCarl);
+console.log(yearJohn);
 
-// variable mutation
-age = 'twenty nine';
-job = 'driver';
+// additional math operators
+var now = 2018;
 
-alert(
-    firstName +
-    ' is a ' + 
-    age + 
-    ' year old ' + 
-    job + 
-    '. Is he married? ' + 
-    isMarried
-);
+console.log(now * 10);
+console.log(now + 10);
+console.log(now / 10);
 
-// ask a user for input
-var lastName = prompt('What is his last name?');
-console.log(firstName + ' ' + lastName);
+// logical operators
+var ageCarl, ageJohn, johnOlder;
+ageCarl = 29;
+ageJohn = 32;
+johnOlder = ageJohn > ageCarl;
+
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'John is older than Carl');
+
+var x;
+console.log(typeof x);
+
+
