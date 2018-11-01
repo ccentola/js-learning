38589083ccda3d318b0312c1fbb0f3771056f4cd
Carl Centola <carl.centola@gmail.com>
2018-11-01 15:55:21 -0400

    variables and data types

--- 2-js-basics/script.js
+++ 2-js-basics/script.js
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+// assign a value to a variable
+var firstName = 'Carl';
+console.log(firstName);
+
+var lastName = 'Centola';
+var age = 29;
+
+var fullAge = true;
+console.log(fullAge);
+
+// unassigned variables return undefined
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
