0124d3a102790998766f54df1f22407d34a23f82
Carl Centola <carl.centola@gmail.com>
2018-11-02 08:55:40 -0400

    the ternary operator and switch statements

--- 2-js-basics/script.js
+++ 2-js-basics/script.js
@@ -1,18 +1,49 @@
-/* Boolean Logic */
+/* The Ternary Operator and Switch Statements */
 
 var firstName = 'Carl';
 var age = 16;
 
-if (age < 13) {
-    console.log(firstName + ' is a boy.');
+// ternary operator
+age >= 18 ? console.log(firstName + ' drinks beer.')
+: console.log(firstName + ' drinks juice.');
 
-} else if (age >= 13 && age <= 20) {
-    // && means "and"
-    console.log(firstName + ' is a teenager.');
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
 
-} else if (age >= 20 && age <= 30) {
-    console.log(firstName + ' is a young man.');
-    
-} else {
-    console.log(firstName + ' is a man.');
+
+// switch statement
+var job = 'instructor';
+
+switch (job) {
+    case 'teacher':
+    case 'instructor': 
+        console.log(firstName + ' teaches kids how to code.');
+        break;
+    case 'driver': 
+        console.log(firstName + ' drives an uber.');
+        break;
+    case 'designer': 
+        console.log(firstName + ' builds beautiful websites.');
+        break;
+    default:
+        console.log(firstName + ' does something else.');
+}
+
+
+// switch with numeric evaluation
+var age = 17;
+
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a young man.');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a man.');
+        break;
+    default:
+        console.log(firstName + ' is a unknown.');
+        break;
 }
