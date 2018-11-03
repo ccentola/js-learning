791379d5e4e4893d27bef7ccfb4922a81055ee5a
Carl Centola <carl.centola@gmail.com>
2018-11-03 08:47:59 -0400

    functions

--- 2-js-basics/script.js
+++ 2-js-basics/script.js
@@ -1,56 +1,25 @@
-/* Coding Challenge 2 */
+/* Functions */
 
-/*
-John and Mike both play basketball on different teams. In the last 3 games, John's team scored 89, 120, ans 103 points. Mike's team scored 116, 94, and 123 points.
-*/
-
-// calculate the average score for each team
-var johnsTeamAvg = (89 + 120 + 103) / 3;
-var mikesTeamAvg = (116 + 94 + 123) / 3;
-
-console.log(johnsTeamAvg);
-console.log(mikesTeamAvg);
-
-
-// print the team with the greater average score
-if (johnsTeamAvg > mikesTeamAvg) {
-    console.log("John's team had the higher average (" + johnsTeamAvg +").");
-} else {
-    console.log("Mike's team had the higher average (" + mikesTeamAvg +").");
+function calculateAge(birthYear) {
+    return 2018 - birthYear;
 }
 
-// change the score to show different winners
-var johnsTeamAvg = (120 + 120 + 103) / 3;
-var mikesTeamAvg = (116 + 94 + 123) / 3;
+var ageCarl = calculateAge(1989);
+var ageMike = calculateAge(1945);
 
-console.log(johnsTeamAvg);
-console.log(mikesTeamAvg);
+console.log(ageCarl);
+console.log(ageMike);
 
-if (johnsTeamAvg > mikesTeamAvg) {
-    console.log("John's team had the higher average (" + johnsTeamAvg +").");
-} else if (mikesTeamAvg > johnsTeamAvg) {
-    console.log("Mike's team had the higher average (" + mikesTeamAvg +").");
-} else {
-    console.log('Both teams had the same average score: (' + mikesTeamAvg +').');
+function yearsUntilRetirement(year,firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(firstName + ' will retire in ' + retirement + ' years.');
+    } else {
+        console.log(firstName + ' is already retired.');
+    } 
 }
 
-// add Mary's team who scored 97,134, and 105
-var marysTeamAvg = (97 + 134 + 105) / 3
 
-console.log(johnsTeamAvg);
-console.log(mikesTeamAvg);
-console.log(marysTeamAvg);
-
-
-if (johnsTeamAvg > mikesTeamAvg && johnsTeamAvg > marysTeamAvg) {
-    console.log("John's team had the higher average (" + johnsTeamAvg +").");
-
-} else if (mikesTeamAvg > johnsTeamAvg && mikesTeamAvg > marysTeamAvg) {
-    console.log("Mike's team had the higher average (" + mikesTeamAvg +").");
-
-} else if (marysTeamAvg > mikesTeamAvg && marysTeamAvg > johnsTeamAvg){
-    console.log("Mary's team had the higher average (" + marysTeamAvg +").");
-    
-} else {
-    console.log('All teams had the same average score: (' + mikesTeamAvg +').');
-}
+yearsUntilRetirement(1989, 'Carl');
