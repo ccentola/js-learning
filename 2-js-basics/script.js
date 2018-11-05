dfbc3164362dd3cfe8c528b708f0699ec9127f1e
Carl Centola <carl.centola@gmail.com>
2018-11-05 10:51:00 -0500

    coding challenege 4

--- 2-js-basics/script.js
+++ 2-js-basics/script.js
@@ -1,18 +1,32 @@
-/* Objects and Methods */
+/* Coding Challenge 4 */
 
-var carl = {
-    birthYear: 1989,
-    family: ["Jane", "Mark", "Bob"],
-    firstName: "Carl",
-    isMarried: true,
-    job: "programmer",
-    lastName: "Centola",
-    calcAge: function() {
-        // using "this" keyword to access object attributes
-        this.age = 2018 - this.birthYear;
+// create objects with John and Mark's properties of name, mass, and height
+
+var john = {
+    name: 'John',
+    mass: 185,
+    height: 72,
+    calcBMI: function() {
+        this.BMI = this.mass / (this.height * this.height);
     }
 };
 
-carl.calcAge();
-console.log(carl);
+var mark = {
+    name: 'John',
+    mass: 206,
+    height: 70,
+    calcBMI: function() {
+        this.BMI = this.mass / (this.height * this.height);
+    }
+};
 
+john.calcBMI();
+mark.calcBMI();
+
+if (john.BMI > mark.BMI) {
+    console.log(john.name + ' has a BMI of ' + john.BMI);
+} else if (mark.BMI > john.BMI) {
+    console.log(mark.name + ' has a BMI of ' + mark.BMI);
+} else {
+    console.log('BMI is the same.');
+}
