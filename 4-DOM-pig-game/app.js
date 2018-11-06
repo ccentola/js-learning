4d2b2801e8ea10cc0582c06fe1d5291fca54c4f8
Carl Centola <carl.centola@gmail.com>
2018-11-06 16:30:09 -0500

    updating scores and changing the active player

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -41,6 +41,27 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
     // update round score if roll was not 1
+    if (dice !== 1) {
+        // add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    } else {
+        // next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
 
 
-})
+});
