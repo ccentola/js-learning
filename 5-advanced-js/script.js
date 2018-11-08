98752ff2cc413efc2a1d4c13b1356fad48ba0239
Carl Centola <carl.centola@gmail.com>
2018-11-07 20:13:46 -0500

    first class functions

--- 5-advanced-js/script.js
+++ 5-advanced-js/script.js
@@ -1,36 +1,38 @@
-// Primitives vs Objects
+// First Class Functions
 
-// primitives
-var a = 23;
-var b = a;
-a = 46;
+var years = [1990, 1989, 1998, 1986, 2001];
 
-console.log(a);
-console.log(b);
+function arrayCalc(arr, fn) {
+    var result = [];
 
-// objects
-var obj1 = {
-    name: 'Carl',
-    age: 30
-};
+    for (var i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i]));
+    }
 
-var obj2 = obj1;
-
-console.log(obj1.age);
-console.log(obj2.age);
-
-// functions
-var age = 29;
-var obj = {
-    name: 'Carl',
-    city: 'Austin'
-}
-function change(a, b) {
-    a = 30;
-    b.city = 'Boston';
+    return result;
 }
 
-change(age, obj);
+function calcAge(x) {
+    return 2016 - x;
+}
 
-console.log(age);
-console.log(obj.city);
+function isAdult(x) {
+    return x >= 18;
+}
+
+function maxHR(x) {
+    if (x >= 18 && x <= 81) {
+        return Math.round(206.9 - (0.67 * x));
+    } else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calcAge);
+console.log(ages);
+
+var fullAges = arrayCalc(years, isAdult);
+console.log(fullAges);
+
+var heartRates = arrayCalc(ages, maxHR);
+console.log(heartRates);
